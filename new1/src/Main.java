98a1206c8b31c3015b177a64401b29b62d379daa
unknown <vakssl@w12-426-38.main.sgu.ru>
2016-01-19 17:42:05 +0300

    task 1 ver2

--- new1/src/Main.java
+++ new1/src/Main.java
@@ -6,35 +6,40 @@ import java.util.Scanner;
 import java.util.Calendar;
 public class Main {
     public static void main(String[] args){
-
-        getYear();
+        getMonth();
     }
 
     private static void getYear() {
         Scanner in = new Scanner(System.in);
         int month = in.nextInt();
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.DATE, 13);
-        calendar.set(Calendar.MONTH,month);
-        for(int i=1800;i<2017;i++){
-            calendar.set(Calendar.YEAR, i);
-            String s = calendar.getDisplayName(Calendar.DAY_OF_WEEK,Calendar.SHORT, Locale.US);
-            if (s.equals("Fri"))
-                System.out.println(i);
+        if (month > 0 && month <= 12) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.DATE, 13);
+            calendar.set(Calendar.MONTH, month-1);
+            for (int i = 1800; i < 2017; i++) {
+                calendar.set(Calendar.YEAR, i);
+                String s = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US);
+                if (s.equals("Fri"))
+                    System.out.println(i);
+            }
         }
+        else System.out.println("Incorrect input");
     }
 
     private static void getMonth() {
         Scanner in = new Scanner(System.in);
         int year = in.nextInt();
+
+        if (year >= 1800 && year <= 2016) {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.YEAR,year);
         for(int i=0;i<12;i++){
         calendar.set(Calendar.MONTH, i);
             calendar.set(Calendar.DATE, 13);
-        String s =calendar.getDisplayName(Calendar.DAY_OF_WEEK,Calendar.SHORT, Locale.US);
+        String s = calendar.getDisplayName(Calendar.DAY_OF_WEEK,Calendar.SHORT, Locale.US);
             if (s == "Fri")
         System.out.println(calendar.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.US));
     }
     }
+    else System.out.println("Incorrect input");}
 }
